598bbf5be7818c601a6bfce876f07ea57f4cbb62
developergodwin01 <godwinovat.go@gmail.com>
2023-09-11 09:27:42 +0100

    stageOne

---
+++ app.js
@@ -0,0 +1,37 @@
+
+let toDay = document.getElementById('currentDayOfTheWeek')
+let UTC = document.getElementById('currentUTCTime')
+let track = document.getElementById('myTrack')
+
+
+
+
+
+
+
+let currentDate = new Date();
+let dayOfWeek = currentDate.getDay();
+let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let currentDay = daysOfWeek[dayOfWeek];
+
+toDay.innerHTML = currentDay;
+// console.log(currentDay);
+
+
+let currentDates = new Date();
+
+// Get the current UTC time in milliseconds
+let utcMilliseconds = currentDates.getTime();
+UTC.innerHTML = utcMilliseconds;
+
+
+// Display the current UTC time in milliseconds
+console.log(utcMilliseconds);
+
+
+// DISPLAYING MY TRACK
+
+track.innerHTML = '<i>FrontEnd</i>'
+
+
+
